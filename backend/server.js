5773b46113de4b60c76be173a65dce9b0806cc4e
samson-16 <samsondemessie@gmail.com>
2025-07-01 12:28:20 -0500

    correction on server.js

--- backend/server.js
+++ backend/server.js
@@ -37,8 +37,8 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
 	app.get('*', (req, res) => {
-		res.sendFile(path.join(__dirname, 'frontend','dist','index.html'));
-	})
+		res.sendFile(path.resolve(__dirname, 'frontend','dist','index.html'));
+	})                         
 }
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
